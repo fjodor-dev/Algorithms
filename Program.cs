4ebc573bbc5313603ef434062d637913db288413
Fjodor <fjodorvanhilst@hotmail.com>
2022-06-14 12:43:44 +0200

    first commit made Bubble and Selection sort

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+
+namespace Algorithms;
+internal class Program
+{
+    public static void Main()
+    {
+        string result = "";
+
+        int[] myArray = new int[400000];
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            myArray[i] = i;
+        }
+        Random random = new Random();
+        myArray = myArray.OrderBy(x => random.Next()).ToArray();
+
+
+        //printArray(myArray);
+
+
+
+        int[] mySortArray1 = new int[myArray.Length];
+        myArray.CopyTo(mySortArray1, 0);
+
+        Stopwatch stopwatch1 = new Stopwatch();
+        stopwatch1.Start();
+        //mySortArray1 = Sort.QuickSort(myArray);
+        stopwatch1.Stop();
+
+        Console.WriteLine();
+        //printArray(mySortArray1);
+
+
+
+
+        int[] mySortArray2 = new int[myArray.Length];
+        myArray.CopyTo(mySortArray2, 0);
+        
+        Stopwatch stopwatch2 = new Stopwatch();
+        stopwatch2.Start();
+        mySortArray2 = Sort.SelectionSort(mySortArray2);
+        stopwatch2.Stop();
+
+
+
+
+        int[] mySortArray3 = new int[myArray.Length];
+        myArray.CopyTo(mySortArray3, 0);
+        Stopwatch stopwatch3 = new Stopwatch();
+        stopwatch3.Start();
+        mySortArray3 = Sort.BubbleSort(mySortArray3);
+        stopwatch3.Stop();
+        
+
+        //Console.WriteLine("Elapsed Time 1 is {0} ms", stopwatch.ElapsedMilliseconds);
+        Console.WriteLine("Elapsed Time 2 is {0} ms", stopwatch2.ElapsedMilliseconds);
+        Console.WriteLine("Elapsed Time 3 is {0} ms", stopwatch3.ElapsedMilliseconds);
+
+
+
+
+
+        void printArray(int[] arr)
+        {
+            result = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i] + ",\n";
+            }
+            Console.WriteLine(result);
+            Console.WriteLine("----------------------------------------------------------");
+        }
+    }
+
+}
+
---
+++ Sort.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal static class Sort
+    {
+
+        public static int[] SelectionSort(int[] arr)
+        {
+            int temp, min, n = arr.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+            return arr;
+        }
+
+        public static void SelectionSortVoid(int[] arr)
+        {
+            int temp, min, n = arr.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+            
+        }
+
+        public static int[] SelectionSortPrint(int[] arr)
+        {
+            int temp, min;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = i;
+                Console.Write($"{i}: ");
+                Console.Write("sorted: ");
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"{j},");
+                }
+
+                Console.Write("unsorted: ");
+                Console.Write($"{min},");
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    Console.Write(arr[j] + ",");
+                    if (arr[j] < arr[min])
+                    {
+
+                        min = j;
+                    }
+                }
+                Console.WriteLine($" min: {min}");
+                Console.WriteLine("");
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+            return arr;
+        }
+
+ 
+        public static int[] BubbleSort(int[] arr)
+        {
+            int n = arr.Length - 1, temp;
+            bool notSorted;
+            do
+            {
+                notSorted = false;
+                for (int i = 0; i < n; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        notSorted = true;
+                    }
+                }
+            }
+            while(notSorted);
+            return arr;
+        }
+
+
+        public static int[] QuickSort(int[] arr)
+        {
+            Random r = new Random();
+            int n = arr.Length, 
+            temp,
+            pivot = r.Next(0, n),
+            pivotVal = arr[pivot];
+            //Console.WriteLine(myArray[pivot]);
+            //for (int i = 0; i < 100; i++)
+            //{
+
+            //    pivot += r.Next(0, n);
+
+            //}
+            //pivot /= 100;
+            Console.WriteLine("pivot index:" + pivot + ", pivot vallue: " + pivotVal);
+            temp = arr[n-1];
+            arr[n-1] = arr[pivot];
+            arr[pivot] = temp;
+
+            int itemL, itemR;
+
+            int i = 0;
+            for (int j = 0; j < n-1; j++)
+            {
+               if(arr[j] < pivotVal)
+               {
+                    i++;
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    Console.WriteLine($"swapt: {arr[i]} with: {arr[j]}");
+                }
+                
+            }
+
+            Console.WriteLine(i);
+            return arr;
+        }
+
+
+
+
+    }
+     
+}
